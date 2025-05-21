306bba1b06efeeaab5e3bafc325e9fdefd69f598
a_nakamura <a_nakamura@LAPTOP-0OVQS0CD>
2025-05-21 11:58:10 +0900

    変更済み

--- src/bean/TestListStudent.java
+++ src/bean/TestListStudent.java
@@ -1,61 +1,34 @@
 package bean;
 
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
 
 public class TestListStudent implements Serializable {
-	private int entYear = 0;
-	private String studentNO = "";
-	private String studentName = "";
-	private String classNum = "";
-	//回数とその得点
-	private Map<Integer, Integer> points = new HashMap<>();
-
-
-	public int getEntYear() {
-		return entYear;
+	private String subjectName;
+	private String subjectCd;
+	private int num;
+	private int point;
+	public String getSubjectName() {
+		return subjectName;
 	}
-	public void setEntYear(int entYear) {
-		this.entYear = entYear;
+	public void setSubjectName(String subjectName) {
+		this.subjectName = subjectName;
 	}
-	public String getStudentNO() {
-		return studentNO;
+	public String getSubjectCd() {
+		return subjectCd;
 	}
-	public void setStudentNO(String studentNO) {
-		this.studentNO = studentNO;
+	public void setSubjectCd(String subjectCd) {
+		this.subjectCd = subjectCd;
 	}
-	public String getStudentName() {
-		return studentName;
+	public int getNum() {
+		return num;
 	}
-	public void setStudentName(String studentName) {
-		this.studentName = studentName;
+	public void setNum(int num) {
+		this.num = num;
 	}
-	public String getClassNum() {
-		return classNum;
+	public int getPoint() {
+		return point;
 	}
-	public void setClassNum(String classNum) {
-		this.classNum = classNum;
-	}
-	public Map<Integer, Integer> getPoints() {
-		return points;
-	}
-	public void setPoints(Map<Integer, Integer> points) {
-		this.points = points;
-	}
-	public String getPoints(int key){
-		Map<Integer,Integer> map = new HashMap<>();
-		map = getPoints();
-		//integer.toStringでintegerからStringにキャスト
-		return Integer.toString(map.get(key));
-		/**
-		 * ためし書き
-		 */
-	}
-	public void putPoint(int key, int value){
-		Map<Integer,Integer> map = new HashMap<>();
-		map = getPoints();
-		map.put(key,value);
-		setPoints(map);
+	public void setPoint(int point) {
+		this.point = point;
 	}
 }
